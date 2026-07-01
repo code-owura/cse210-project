21deac51b9d167872803ac1538febde7df81f1f6
Kwadwo Adu Nsiah Ahenkan <>
2026-07-01 22:52:34 +0000

    Finished Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (number >= 90)
+        {
+            letter = "B";
+        }
+
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
