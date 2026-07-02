59a3dc414cea03fdfb077fcfabf8df1ceeff6a11
Kwadwo Adu Nsiah Ahenkan <>
2026-07-02 03:08:32 +0000

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Part 1 requirement, user gives the magic number
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        string level = "";
+        int digit = 0;
+        while (digit != number)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            digit = int.Parse(guess);
+
+            if (digit < number)
+            {
+                level = "Higher";
+            }
+
+            else if (digit > number)
+            {
+                level = "Lower";
+            }
+
+            else
+            {
+                level = "You guessed it!";
+            }
+
+            Console.WriteLine($"{level}");
+        }
     }
 }
