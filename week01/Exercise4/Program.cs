130c20818a1392d2b8fe32de8f57c88b095bfbee
Kwadwo Adu Nsiah Ahenkan <>
2026-07-03 21:46:09 +0000

    Finished Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,56 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+
+            Console.Write("What is your number? ");
+            string seriesNumber = Console.ReadLine();
+            number = int.Parse(seriesNumber);
+
+            if (number != 0)
+            {
+
+                numbers.Add(number);
+
+            }
+        }
+
+            int sum = 0;
+
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+
+            float average = (float)sum / numbers.Count;
+
+            Console.WriteLine($"The average is: {average}");
+
+
+            int max = numbers[0];
+
+            foreach (int n in numbers)
+            {
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+
+            Console.WriteLine($"The largest number is: {max}");
+
+
     }
 }
