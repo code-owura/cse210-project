eda1fd0c511b42e7099e40ce762f1594918454f6
Kwadwo Adu Nsiah Ahenkan <>
2026-08-12 03:03:48 +0000

    Week07 Assignment

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,36 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        string activityName = this.GetType().Name;
+        return $"{_date:dd MMM yyyy} {activityName} ({_minutes} min) - " +
+               $"Distance {GetDistance():F1} miles, " +
+               $"Speed {GetSpeed():F1} mph, " +
+               $"Pace: {GetPace():F1} min per mile";
+    }
+}
